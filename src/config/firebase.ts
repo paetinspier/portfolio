9b22e6549d997eb0bfab422946d88fba96a32749
paetinspier <paetin.nelson@gmail.com>
2023-05-12 20:35:26 -0700

    wip

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,14 +6,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 let firebaseCredentials: any;
 if (process.env.VERCEL_ENV) {
-	if (process.env.VERCEL_ENV === "production") {
-        firebaseCredentials = {
-            apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        };
-	}
+	firebaseCredentials = {
+		apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	};
 } else {
 	firebaseCredentials = {
 		apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
@@ -22,7 +20,7 @@ if (process.env.VERCEL_ENV) {
 		storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 	};
 }
-
+console.log(firebaseCredentials)
 const app = initializeApp(firebaseCredentials);
 export const auth = getAuth();
 export const firestore = getFirestore(app);
